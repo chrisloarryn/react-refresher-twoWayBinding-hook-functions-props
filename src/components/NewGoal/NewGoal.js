25a3dc38a5efd487f0b0a44ec74e61ded0b12b4e
chrisloarryn <cristobal.contreras11@inacapmail.cl>
2020-05-01 19:49:10 -0400

    update / img with changes and features implemented in this lecture

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Css classes
 import './NewGoal.css'
 const NewGoal = props => {
-  const addGoalHandler = (event) => {
+  // let enteredText = ''
+  const [enteredText, setEnteredText] = useState('')
+
+  const addGoalHandler = event => {
     event.preventDefault()
     const newGoal = {
-        id: Math.random().toString(),
-        text: 'My new goal'
+      id: Math.random().toString(),
+      text: enteredText
     }
     console.log(newGoal)
+    setEnteredText('')
+    if (!newGoal.text) return
     props.onAddGoal(newGoal)
+  }
+
+  const textChangeHandler = event => {
+    // console.log(event);
+    setEnteredText(event.target.value)
   }
 
   return (
     <form className='new-goal' onSubmit={addGoalHandler}>
-      <input type='text' />
+      <input type='text' value={enteredText} onChange={textChangeHandler} />
       <button type='submit'>Add Goal</button>
     </form>
   )
