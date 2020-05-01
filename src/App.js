b0cf9d4d7ec12dc2b6022efa5c6d8ab62b00b9f0
chrisloarryn <cristobal.contreras11@inacapmail.cl>
2020-05-01 19:12:21 -0400

    Add new goal, listing goals

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react'
+import GoalList from './components/GoalList/GoalList'
+import NewGoal from './components/NewGoal/NewGoal'
 
+// Css styles
+import './App.css'
+
+// const App = () => {
+//   return <h1 title="this works!">Hi, <span>this</span> is ReactJS!</h1>;
+// };
 const App = () => {
-  return <h1>A React App!</h1>;
-};
+  const [courseGoals, setCourseGoals] = useState([
+    { id: 'cg1', text: 'Finish the Course' },
+    { id: 'cg2', text: 'Learn all about The Course Main Topic' },
+    { id: 'cg3', text: 'Help other students in the Course Q&A' }
+  ])
 
-export default App;
+  const addNewGoalHandler = newGoal => {
+    // setCourseGoals(courseGoals.concat(newGoal))
+    setCourseGoals(prevCourseGoals => prevCourseGoals.concat(newGoal))
+  }
+
+  return (
+    <div className='course-goals'>
+      <h2>Course Goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
+    </div>
+  )
+}
+
+export default App
